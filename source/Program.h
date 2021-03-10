604ce095a62cb6698b7cbfd7f9a43618694421a4
Kacu <k.ptychu@gmail.com>
2021-03-10 20:22:23 +0100

    new operation: Program::waitForUserInput

--- source/Program.h
+++ source/Program.h
@@ -1,31 +1,43 @@
 #pragma once
 
+#include <iostream>
 #include <string>
+
 
 #include "Event.h"
 
 
 class Program {
-    const bool _isInitializable;
+    const bool _isNotInitializable;
     const std::string _targetFolder;
 
 
 public:
     Program(const int argc, const char* const * const argv):
-        _isInitializable(argc > 1),
+        _isNotInitializable(argc < 2),
         _targetFolder(initTargetFolder(argv))
     {}
     
+    void execute() {
+        if (_isNotInitializable) {
+            std::cout << "No argument was given.\n";
+            waitForUserInput();
+        }
+        
+    }
 
 
 private:
     std::string initTargetFolder(const char* const * const argv) {
-        if (_isInitializable) {
-            return argv[1];
+        if (_isNotInitializable) {
+            return std::string();   
         }
-        return std::string();   
+        return argv[1];
     }
 
-
+    void waitForUserInput() {
+        std::cout << "Press [enter] to continue.";
+        std::cin.get();
+    }
 };
 
