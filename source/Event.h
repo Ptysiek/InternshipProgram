c9e666810b15a511dfa383a10af4f0617859666b
Kacu <k.ptychu@gmail.com>
2021-03-10 19:38:18 +0100

    new class: Event

---
+++ source/Event.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <sstream>
+#include <string>
+
+
+struct Event {
+    enum Type {Created, Deleted, Edited, Renamed};
+    
+    const Type _type;
+    const std::string _fileLocation;
+    const bool _isFolder;
+
+    std::string log() const {
+        std::stringstream result;
+        result << (_isFolder)? "Folder " : "File "
+            << "\'" << _fileLocation << "\' "
+            << "is " << to_string(_type) << ".";
+        return result.str();
+    }
+
+private:
+    std::string to_string(const Type type) {
+        switch(type) {
+        case Created:
+            return "Created";
+        case Deleted:
+            return "Deleted";
+        case Edited:
+            return "Edited";
+        default: 
+            return "Renamed";
+        }
+    }
+};
+
--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,10 @@
 
 
+//int main(int argc, char** argv) {
 int main() {
+    
 
     return 0;
 }
+
+
