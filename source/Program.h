cac83676cbb9649465314aa5d408fc2e9435a0ae
Kacu <k.ptychu@gmail.com>
2021-03-10 23:48:28 +0100

    new operation: Program::validateTargetFolder

--- source/Program.h
+++ source/Program.h
@@ -1,42 +1,76 @@
 #pragma once
 
+#include <exception>
+#include <experimental/filesystem>  
+    // Neither GCC(7.5 - 2019) nor Clang(6.0.0 - 2018) has a <filesystem> yet.
+    // Needs to be linked with static library: 'libstdc++fs.a' [-lstdc++fs].
+    // Or You can use GCC(9 - 2020)
 #include <iostream>
 #include <string>
 
-
 #include "Event.h"
+#include "InputOutput.h"
 
 
 class Program {
-    const bool _isNotInitializable;
+    using Path = std::experimental::filesystem::directory_entry;
+
+    const bool _isInitializable;
     const std::string _targetFolder;
+    Path _path;
 
 
 public:
-    Program(const int argc, const char* const * const argv):
-        _isNotInitializable(argc < 2),
-        _targetFolder(initTargetFolder(argv))
+    explicit Program(const int argc, const char* const * const argv):
+        _isInitializable(argc > 1),
+        _targetFolder(initTargetFolder(argv)),
+        _path(_targetFolder)
     {}
     
     void execute() {
-        if (_isNotInitializable) {
-            std::cout << "No argument was given.\n";
+        if (!validateTargetFolder()) {
             waitForUserInput();
+            return;
         }
         
+        std::cout << _path << "\n\n";
+
     }
 
 
 private:
     std::string initTargetFolder(const char* const * const argv) {
-        if (_isNotInitializable) {
-            return std::string();   
+        if (_isInitializable) {
+            return argv[1];
         }
-        return argv[1];
+        return std::string();   
+    }
+
+    bool validateTargetFolder() {
+        using namespace std::experimental;
+        
+        if (!_isInitializable) {
+            std::cerr << "No argument was given.\n";
+            return false;
+        }
+        const auto& status = _path.status();
+
+        if (!filesystem::status_known(status)) {
+            throw std::logic_error("Program::validateTargetFolder: Unknown Status.");
+        }
+        if (!filesystem::exists(status)) {
+            std::cerr << "Specified path does not exist. \'" << _targetFolder << "\'\n";
+            return false;
+        }
+        if (!filesystem::is_directory(status)) {
+            std::cerr << "Specified path is not a directory. \'" << _targetFolder << "\'\n";
+            return false;
+        }
+        return true;
     }
 
     void waitForUserInput() {
-        std::cout << "Press [enter] to continue.";
+        std::cerr << "Press [enter] to continue.   ";
         std::cin.get();
     }
 };
